1bf43833973a3b20c009b10b656d76b32a0a6bd8
stefftaelman <stefftaelman@gmail.com>
2021-10-19 08:54:00 +0200

    removed hypervector structs inclusion

--- src/HyperdimensionalComputing.jl
+++ src/HyperdimensionalComputing.jl
@@ -7,7 +7,6 @@ export multiply, aggregate, rotate
 export encode_alphabet, encode_sequence
 export cosine_predict
 
-include("hypervectors.jl")
 include("operations.jl")
 include("encoding.jl")
 include("predictors.jl")
