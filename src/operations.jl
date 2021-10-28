30813934c35bc1f9f19b622e8abc120b22a3cdb9
michielstock <michielfmstock@gmail.com>
2021-10-28 11:01:03 +0200

    🚧 operations

--- src/operations.jl
+++ src/operations.jl
@@ -2,57 +2,57 @@
 operations.jl; This file implements operations that can be done on hypervectors to enable them to encode text-based data.
 =#
 
+# Remark: use element-wise reduce, maybe using LazyArrays?
+
+
+#=
+
+| Operation            | symbol | remark                                                                                                          |
+| -------------------- | ------ | --------------------------------------------------------------------------------------------------------------- |
+| Bundling/aggregating | `+`    | combines the information of two vectors into a new vector similar to both                                       |
+| Binding              | `*`    | mapping, combines the two vectors in something different from both, preserves distance, distributes of bundling |
+| Shifting             | `Π`    | Permutation (in practice cyclic shifting), distributes over addition, conserves distance                        |
+=#
 
 """
-This function takes two hypervectors as input and returns a vector that is dissimilar to both. For bipolar vectors, this is an elementwise multiplication.
+    grad2bipol(x::Number)
+
+Maps a graded number in [0, 1] to the [-1, 1] interval.
 """
-function multiply(hv1::Vector{Int}, hv2::Vector{Int})
-    return hv1 .* hv2
+grad2bipol(x::Number) = 2x - one(x)
+
+
+"""
+bipol2grad(x::Number)
+
+Maps a bipolar number in [-1, 1] to the [0, 1] interval.
+"""
+bipol2grad(x::Number) = (x + one(x)) / 2
+
+
+three_pi(x, y) = x * y / (x * y + (one(x) - x) * (one(y) - y))
+fuzzy_xor(x, y) = x + y - x * y
+
+three_pi_bipol(x, y) = grad2bipol(three_pi(bipol2grad(x), bipol2grad(y)))
+fuzzy_xor_bipol(x, y) = grad2bipol(fuzzy_xor(bipol2grad(x), bipol2grad(y)))
+
+
+# AGGREGATION
+# -----------
+
+# BINDING
+# -------
+
+
+# SHIFTING
+# --------
+
+Base.circshift!(hdv::AbstractHDV, k) = (hdv.offset += k)
+
+function Base.circshift(hdv::AbstractHDV, k)
+    hdv = copy(hdv)
+    hdv.offset += k
+    return hdv
 end
 
-
-"""
-This function takes two hypervectors as input and returns a vector that is dissimilar to both. For binary vectors, this is an X-OR operation.
-
-QUESTION: does this actually work elementwise?
-"""
-function multiply(hv1::BitVector, hv2::BitVector)
-    return hv1 .⊻ hv2
-end
-
-
-"""
-This functions aggragates two binary hypervectors to a new one that is similar to both. For bipolar vectors, this is possible by summing the elements at each position and returning their sign.
-"""
-function add(hv1::Vector{Int}, hv2::Vector{Int})
-    return hv1 + hv2
-end
-
-
-""" 
-This functions aggragates two binary hypervectors to a new one that is similar to both. For binary vectors (where averaging 0 and 1 is not possible), this is done by randomly picking an element from each at every position. 
-This way, two ones return a one, two zeros return a zero, and a one and a zero have a 50% chance of returning either.
-
-DISCLAIMER: this solution to averaging binary vectors is a bit iffy, but might still work.
-"""
-function add(hv1::BitVector, hv2::BitVector)
-    return convert(BitVector, [rand([hv1[x], hv2[x]]) for x in 1:HYPERVECTOR_DIM])
-end
-
-
-"""
-This function rotates a bipolar hypervector by a given degree in order to encode its position. 
-
-DISCLAIMER: still to be benchmarked. This might not be the fastest way to do this as circshift creates new vectors and allocates extra memory to a vector that in this case will be instantly discarded.
-"""
-function rotate(hv::Vector, degree::Int)
-    return circshift(hv, -degree)
-end
-
-
-"""
-This function rotates a binary hypervector by a given degree in order to encode its position. 
-"""
-function rotate(hv::BitVector, degree::Int)
-    return hv >> -degree
-end
+Π(hdv::AbstractHDV, k) = circshift(hdv, k)
