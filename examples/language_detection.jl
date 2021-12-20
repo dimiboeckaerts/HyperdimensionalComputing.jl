2566342c094498611f98051d70848d2fd455ac42
stefftaelman <stefftaelman@gmail.com>
2021-12-20 09:18:41 +0100

    updated language detection example

--- examples/language_detection.jl
+++ examples/language_detection.jl
@@ -1,12 +1,9 @@
 #=
 language_detection.jl; this script shows the application of hypervectors to a language_detection problem.
 =#
-
-using CSV, DataFrames, HyperdimensionalComputing
-
-
-# set 26 letter alphabet and set up preprocessing to fit texts to this format
-alphabet_hv = encode_items(vcat('a':'z', ' '))
+using Pkg
+Pkg.activate(".")
+using CSV, DataFrames, HyperdimensionalComputing, MLJ
 
 
 """
@@ -54,43 +51,30 @@ function language_preprocessing(text::String)
 end
 
 
-labelled_languages_df = DataFrame(CSV.File("data/labelled_languages_many.csv"))
+# load in and preprocess training paragraphs from project Gutenberg
+labelled_languages_df = DataFrame(CSV.File("data/labelled_languages_train.csv"))
 insertcols!(labelled_languages_df, 2, :"PREPROC"=>Vector{String}(undef, size(labelled_languages_df, 1)))
-for i in 1:size(labelled_languages_df, 1)
-        labelled_languages_df[i, "PREPROC"] = language_preprocessing(labelled_languages_df[i, "TEXT"])
+labelled_languages_df[!, "PREPROC"] = [language_preprocessing(labelled_languages_df[i, "TEXT"]) for i in 1:size(labelled_languages_df, 1)]
+
+
+# load in and preprocess testing sentences from tatoeba.org
+examples_df = DataFrame(CSV.File("data/labelled_languages_test.csv"))
+insertcols!(examples_df, 2, :"PREPROC"=>Vector{String}(undef, size(examples_df, 1)))
+examples_df[!, "PREPROC"] = [language_preprocessing(examples_df[i, "TEXT"]) for i in 1:size(examples_df, 1)]
+
+
+for vec in [BipolarHDV, BinaryHDV, GradedHDV, RealHDV]
+    println(vec)
+
+    # encode both train and test data
+    alphabet = Dict(c=>vec() for c in vcat('a':'z', ' '))
+    encoded_references = [sequence_embedding(ref, alphabet, 3) for ref in labelled_languages_df[!, "PREPROC"]]
+    encoded_examples = [sequence_embedding(ex, alphabet, 3) for ex in examples_df[!, "PREPROC"]]
+
+    # predict
+    centers = train(labelled_languages_df[!, "LANGUAGE"], encoded_references)
+    test_predictions = [HyperdimensionalComputing.predict(hv, centers) for hv in encoded_examples]
+
+    # evaluate performance
+    println(string("weighted F1-score: ", multiclass_f1score(test_predictions, examples_df[!, "LANGUAGE"])))
 end
-
-first(labelled_languages_df, 5)
-
-# get some example languages to detect
-examples = ["Hyperdimensionaal rekenen: volgens enkele papers zou dit representaties kunnen genereren die even krachtig zijn als word2vec!", 
-            "Ceci n'est pas une pipe. Liberté, égalité, fraternité.", 
-            "Para bailar la bamba, para bailar la bamba se necesita una poca de gracia. Una poca de gracia pa' mi pa' ti y arriba y arriba. Ah y arriba y arriba por ti seré, por ti seré, por ti seré. Yo no soy marinero. Yo no soy marinero, soy capitán, Soy capitán, soy capitán.",
-            "We're no strangers to love, you know the rules and so do I. A full commitment's what I'm thinking of, you wouldn't get this from any other guy.", 
-            "My tailor is rich, but my english is poor.", 
-            "Der Begriff ”wahrscheinlich” wird im Alltag in verschiedenen Situationen verwendet, hat dabei auch unterschiedliche Bedeutung.", 
-            "Véletlen módszerrel bebizonyította minden n és s értékére n-kromatikus s kerületű (legrövidebb kör hossza) gráf létezését."]
-example_labels = ["dutch", "french", "spanish", "english", "english", "german", "hungarian"]
-examples_preproc = [language_preprocessing(ex) for ex in examples]
-
-
-# encode both train and test data
-encoded_references = Dict((labelled_languages_df[idx, 1]=>encode_sequence(labelled_languages_df[idx, 2], alphabet_hv, k=3) for idx in 1:size(labelled_languages_df, 1)))
-encoded_examples = [encode_sequence(ex, alphabet_hv, k=3) for ex in examples_preproc]
-
-# predict
-println("Bipolar vector predictions:")
-println(cosine_predict(encoded_examples, encoded_references))
-
-
-# now let's try with binary hypervectors
-bit_alphabet_hv = encode_items(vcat('a':'z', ' '), vectortype="binary")
-
-bit_encoded_references = Dict((labelled_languages_df[idx, 1]=>encode_sequence(labelled_languages_df[idx, 2], bit_alphabet_hv, k=3) for idx in 1:size(labelled_languages_df, 1)))
-bit_encoded_examples = [encode_sequence(ex, bit_alphabet_hv, k=5) for ex in examples_preproc]
-
-println("Binary vector predictions:")
-println(cosine_predict(bit_encoded_examples, bit_encoded_references))
-
-println("Ground truth:")
-println(example_labels)
